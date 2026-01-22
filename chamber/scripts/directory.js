c2e2f877abf2c363202df14080680c05d39788b5
Giorubio27 <Giorubio27@gmail.com>
2026-01-22 09:45:17 -0700

    hoopla

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -3,12 +3,28 @@ document.getElementById("currentyear").innerHTML = new Date().getFullYear();
 
 const hambutton = document.querySelector('#menu');
 const navigation = document.querySelector('.hammer');
-const cards = document.querySelector('.bcards');
+const cards = document.querySelector('#company-displays');
+const gridbutton = document.querySelector('#grid');
+const listbutton = document.querySelector('#list');
+const display = document.querySelector('article');
+
+gridbutton.addEventListener('click', () => {
+    display.classList.add('bcards');
+    display.classList.remove('list');
+});
+
+listbutton.addEventListener('click', showList);
+
+function showList() {
+    display.classList.add('list');
+    display.classList.remove('grid');
+}
+
 
 hambutton.addEventListener('click', () => {
     hambutton.classList.toggle('primetime');
     navigation.classList.toggle('primetime');
-})
+});
 
 async function getCompanyData() {
     const response = await fetch('data/members.json')
@@ -36,11 +52,6 @@ const displayCompanyCard = (companies) => {
         image.setAttribute('src', company.img);
         image.setAttribute('alt', 'companyphoto')
         membership.innerHTML = `Current Membership is: ${company.membership}`;
-        card.style.border = '1px solid black';
-        card.style.width = '300px';
-        card.style.height = '300px';
-        card.style.backgroundColor = 'lightblue';
-        card.style.boxShadow = '0 0 10px black'
 
         card.appendChild(name);
         card.appendChild(image);
