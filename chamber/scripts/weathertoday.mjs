491291b4076f1f4b7b9461577628a60c2e33d3fd
Giorubio27 <Giorubio27@gmail.com>
2026-01-28 13:40:27 -0700

    http

--- chamber/scripts/weathertoday.mjs
+++ chamber/scripts/weathertoday.mjs
@@ -6,7 +6,7 @@ const myIcon = document.querySelector('#weather-icon');
 const myKey = '1ba899a2af720c65ec0ea26ecb83b2df'
 const lat = 36.15139252091463
 const lon = -115.17088162858838
-const url = `//api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${myKey}&units=imperial`;
+const url = `https//api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${myKey}&units=imperial`;
 
 
 
