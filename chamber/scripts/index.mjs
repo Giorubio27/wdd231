dd2f6da56103b8f8aa898c0c447e732e1b533b6c
Giorubio27 <Giorubio27@gmail.com>
2026-01-28 13:54:27 -0700

    hambutton

--- chamber/scripts/index.mjs
+++ chamber/scripts/index.mjs
@@ -5,6 +5,13 @@ import { getEventsData } from './events.mjs';
 
 document.getElementById("last_modified").innerHTML = document.lastModified;
 document.getElementById("currentyear").innerHTML = new Date().getFullYear();
+const hambutton = document.querySelector('#menu');
+const navigation = document.querySelector('.hammer');
+
+hambutton.addEventListener('click', () => {
+    hambutton.classList.toggle('primetime');
+    navigation.classList.toggle('primetime');
+});
 
 
 
