749671ac046dfa94de1c618d2bf9b0c240ede1dc
Giorubio27 <Giorubio27@gmail.com>
2026-02-05 16:55:58 -0700

    added modals

--- chamber/scripts/dialogue.mjs
+++ chamber/scripts/dialogue.mjs
@@ -1,29 +1,48 @@
+const dialogue = document.querySelector('#member-info');
+const modalButton = document.querySelector('#modal');
+const closeModal = document.querySelector('#close-modal');
+
+closeModal.addEventListener('click', () => {
+    dialogue.close();
+});
+    
+
+
+
 const memberCards = document.querySelector('#membercards');
 
-export async function getCompanyData() {
+export async function getMembershipData() {
     const response = await fetch('data/memberships.json')
-    const data = await response.json();
+    const memberdata = await response.json();
 
-    const allMemberships = data.memberships;
+    const allMemberships = memberdata.memberships;
 
 
-    cards.innerHTML = "";
+    memberCards.innerHTML = "";
 
     displayMemberships(allMemberships);
 }
 
 export const displayMemberships = (memberships) => {
-    memberships.foreach((membership) => {
+    memberships.forEach((membership) => {
         let card = document.createElement('section');
         let name = document.createElement('h3');
-        let button = document.createElement('a');
+        let button = document.createElement('button');
 
         name.innerHTML = membership.name;
         button.innerHTML = membership.button;
+        button.addEventListener('click', () => {
+            modalButton.innerHTML = `
+            <h2>${membership.name}</h2>
+            <p>${membership.perks}</P>
+            <p>Price: ${membership.cost}</P>`;
+            dialogue.showModal();
+        });
 
         card.appendChild(name);
         card.appendChild(button);
 
         memberCards.appendChild(card);
-    })
+    });
 }
+getMembershipData();
