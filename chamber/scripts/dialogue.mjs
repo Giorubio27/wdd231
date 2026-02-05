ec498445749b871aa1898ec9dbb35d4b38b3d5a1
Giorubio27 <Giorubio27@gmail.com>
2026-02-05 15:05:04 -0700

    opa opa# Your branch is up to date with 'origin/main'.

--- chamber/scripts/dialogue.mjs
+++ chamber/scripts/dialogue.mjs
@@ -1,1 +1,29 @@
-const memberCards = document.querySelector('#membercards')
+const memberCards = document.querySelector('#membercards');
+
+export async function getCompanyData() {
+    const response = await fetch('data/memberships.json')
+    const data = await response.json();
+
+    const allMemberships = data.memberships;
+
+
+    cards.innerHTML = "";
+
+    displayMemberships(allMemberships);
+}
+
+export const displayMemberships = (memberships) => {
+    memberships.foreach((membership) => {
+        let card = document.createElement('section');
+        let name = document.createElement('h3');
+        let button = document.createElement('a');
+
+        name.innerHTML = membership.name;
+        button.innerHTML = membership.button;
+
+        card.appendChild(name);
+        card.appendChild(button);
+
+        memberCards.appendChild(card);
+    })
+}
