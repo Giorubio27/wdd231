2a99b602a950f6f582eecb5c333e98a3100f1457
Giorubio27 <Giorubio27@gmail.com>
2026-02-10 05:33:26 -0700

    gridarea update

--- chamber/scripts/items.mjs
+++ chamber/scripts/items.mjs
@@ -1,3 +1,10 @@
+const itemDialogue = document.querySelector('#item-info');
+const itemModalButton = document.querySelector('#item-modal');
+const itemCloseModal = document.querySelector('#close-item-modal');
+
+itemCloseModal.addEventListener('click', () => {
+    itemDialogue.close();
+});
 export const itemCards = document.querySelector('.items');
 
 export async function GetItemData() {
@@ -16,16 +23,26 @@ export const displayItemCards = (items) => {
         let itemName = document.createElement('h3');
         let itemDescription = document.createElement('p');
         let itemImage = document.createElement('img');
+        let itemButton = document.createElement('button');
 
         itemName.innerHTML = item.name;
         itemDescription.innerHTML = item.description;
         itemImage.setAttribute('src', item.image_url);
+        itemButton.innerHTML = `Learn More`;
+        itemButton.addEventListener('click', () => {
+            itemModalButton.innerHTML = `
+            <h2>${item.name}</h2>
+            <p>Description: ${item.description}</P>
+            <p>Address: ${item.address}</P>`;
+            itemDialogue.showModal();
+        });
         
         
 
         itemCard.appendChild(itemName);
         itemCard.appendChild(itemImage);
         itemCard.appendChild(itemDescription);
+        itemCard.appendChild(itemButton);
         
 
         itemCards.appendChild(itemCard);
