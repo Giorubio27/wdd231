621f3590b09e99a6f4416dbbecdbacd3fa346f3c
Giorubio27 <Giorubio27@gmail.com>
2026-02-10 06:26:22 -0700

    added visit mjs

--- chamber/scripts/visit.mjs
+++ chamber/scripts/visit.mjs
@@ -1,14 +1,17 @@
 export const visitPage = document.querySelector(".visit")
 
 export let numVisits = number(window.localStorage.getItem("numVisits - 1s")) || 0;
+let timeBetweenVisits = 
 
 numVisits++;
 
  if (numVisits !== 0) {
     visitPage.textContent = `Number of Visits: ${numVisits}`;
 
-} else {
-    visitPage.textContent = `Welcome to our Page!`
+} else if (numVisits !== 0 && ) {
+    visitPage.textContent = `Welcome to our Page. Let me know if you have any questions!`
+ } else {
+     
 }
 
 window.localStorage.setItem("numVisits - 1s", numVisits);
